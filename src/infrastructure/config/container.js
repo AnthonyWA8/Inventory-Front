08894785b3ac99aa8d9033918be91419a03d2f80
Anthony <sebastianagva@gmail.com>
2026-04-10 12:20:03 -0500

    front

---
+++ src/infrastructure/config/container.js
@@ -0,0 +1,12 @@
+import CreateProduct         from "../../application/usecases/CreateProduct.js";
+import GetAllProducts        from "../../application/usecases/GetAllProducts.js";
+import GetProductBySku       from "../../application/usecases/GetProductBySku.js";
+import DeleteProduct         from "../../application/usecases/DeleteProduct.js";
+import ApiProductRepository from "../repositories/ApiProductRepository.js";
+
+const repository = new ApiProductRepository();
+
+export const createProduct   = new CreateProduct(repository);
+export const getAllProducts   = new GetAllProducts(repository);
+export const getProductBySku = new GetProductBySku(repository);
+export const deleteProduct   = new DeleteProduct(repository);
